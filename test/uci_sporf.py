4001c349174566590586916d90685f078f69b015
parthgvora <parthgvora@gmail.com>
2020-11-12 22:12:21 -0500

    uci testing went well :)

--- test/uci_sporf.py
+++ test/uci_sporf.py
@@ -39,6 +39,7 @@ def test(data_file, reps, n_trees,
         y_hat = pl.predict(X_test, task_id=0)
 
         acc = np.sum(y_test == y_hat) / len(y_test)
+        print(acc)
 
         chance_pred = 1 / n_classes
         kappa[i] = (acc - chance_pred) / (1 - chance_pred)
@@ -49,10 +50,23 @@ def load_data(data_file):
 
     df = pd.read_csv(data_file)
 
-    if "Hill_Valley" in data_file:
+    if "Hill_Valley" in data_file or "tae" in data_file:
         X = df[df.columns[:-1]].to_numpy()
         y = df[df.columns[-1]].to_numpy()
     
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, shuffle=True, stratify=y) 
+
+    if "balance" in data_file:
+
+        X = df[df.columns[1:]].to_numpy()
+        
+        labels = df.B.to_list()
+        y = np.zeros(len(labels))
+        
+        label_dict = {"B": 0, "L": 1, "R": 2}
+        for i, l in enumerate(labels):
+            y[i] = label_dict[l]
+        
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, shuffle=True, stratify=y) 
 
     if "ecoli" in data_file:
@@ -76,24 +90,22 @@ def load_data(data_file):
 
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, stratify=y) 
 
-    if "promoters" in data_file:
-        print(df)
-
-
     return X_train, X_test, y_train, y_test, len(np.unique(y))
 
 def main():
    
     data_files = [
                     "https://archive.ics.uci.edu/ml/machine-learning-databases/hill-valley/Hill_Valley_without_noise_Training.data",
+                    "https://archive.ics.uci.edu/ml/machine-learning-databases/hill-valley/Hill_Valley_with_noise_Training.data",
                     "https://archive.ics.uci.edu/ml/machine-learning-databases/ecoli/ecoli.data",
-                    "https://archive.ics.uci.edu/ml/machine-learning-databases/molecular-biology/promoter-gene-sequences/promoters.data"
+                    "https://archive.ics.uci.edu/ml/machine-learning-databases/tae/tae.data",
+                    "https://archive.ics.uci.edu/ml/machine-learning-databases/balance-scale/balance-scale.data"
                  ]
 
     # Parameters
     max_depth = 10
-    feature_combinations = 2
-    density = 0.1
+    feature_combinations = 1.5
+    density = 0.5
 
     reps = 5
     n_trees = 10
@@ -103,15 +115,16 @@ def main():
                              "feature_combinations" : feature_combinations,
                              "density" : density}}
 
-    RF_kappa, RF_err = test(data_files[1], reps, n_trees,
+    RF_kappa, RF_err = test(data_files[-1], reps, n_trees,
                             default_transformer_class=TreeClassificationTransformer,
                             default_transformer_kwargs=rf_kwargs)
 
-    #OF_kappa, OF_err = test(data_files[1], reps, n_trees,
-    #                        default_transformer_class=ObliqueTreeClassificationTransformer,
-    #                        default_transformer_kwargs=of_kwargs)
+    OF_kappa, OF_err = test(data_files[-1], reps, n_trees,
+                            default_transformer_class=ObliqueTreeClassificationTransformer,
+                            default_transformer_kwargs=of_kwargs)
+    
     print("RF kappa, RF err:", RF_kappa, RF_err)
-    #print("OF kappa, OF err:", OF_kappa, OF_err)
+    print("OF kappa, OF err:", OF_kappa, OF_err)
 
 if __name__ == "__main__":
     main()
