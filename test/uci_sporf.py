aea5bc1e09b6ee77fc9706e3aad53c499aec1a5a
parthgvora <parthgvora@gmail.com>
2020-11-13 14:28:43 -0500

    same output as sporf for hill valley no noise

--- test/uci_sporf.py
+++ test/uci_sporf.py
@@ -54,7 +54,7 @@ def load_data(data_file):
         X = df[df.columns[:-1]].to_numpy()
         y = df[df.columns[-1]].to_numpy()
     
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, shuffle=True, stratify=y) 
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, shuffle=True, stratify=y) 
 
     if "balance" in data_file:
 
@@ -67,28 +67,7 @@ def load_data(data_file):
         for i, l in enumerate(labels):
             y[i] = label_dict[l]
         
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, shuffle=True, stratify=y) 
-
-    if "ecoli" in data_file:
-
-        X = np.zeros((df.values.shape[0], 7))
-        y = np.zeros(df.values.shape[0])
-
-        seen = []
-        seen_count = -1
-        for i in range(df.values.shape[0]):
-            entry = df.values[i][0].split()
-            data = entry[1:-1]
-            label = entry[-1]
-
-            if label not in seen:
-                seen.append(label)
-                seen_count += 1
-
-            X[i] = np.array([float(i) for i in data])
-            y[i] = seen_count
-
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, stratify=y) 
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, shuffle=True, stratify=y) 
 
     return X_train, X_test, y_train, y_test, len(np.unique(y))
 
@@ -97,15 +76,14 @@ def main():
     data_files = [
                     "https://archive.ics.uci.edu/ml/machine-learning-databases/hill-valley/Hill_Valley_without_noise_Training.data",
                     "https://archive.ics.uci.edu/ml/machine-learning-databases/hill-valley/Hill_Valley_with_noise_Training.data",
-                    "https://archive.ics.uci.edu/ml/machine-learning-databases/ecoli/ecoli.data",
                     "https://archive.ics.uci.edu/ml/machine-learning-databases/tae/tae.data",
                     "https://archive.ics.uci.edu/ml/machine-learning-databases/balance-scale/balance-scale.data"
                  ]
 
     # Parameters
     max_depth = 10
-    feature_combinations = 1.5
-    density = 0.5
+    feature_combinations = 2
+    density = 0.01
 
     reps = 5
     n_trees = 10
@@ -115,15 +93,15 @@ def main():
                              "feature_combinations" : feature_combinations,
                              "density" : density}}
 
-    RF_kappa, RF_err = test(data_files[-1], reps, n_trees,
-                            default_transformer_class=TreeClassificationTransformer,
-                            default_transformer_kwargs=rf_kwargs)
+    #RF_kappa, RF_err = test(data_files[-1], reps, n_trees,
+    #                        default_transformer_class=TreeClassificationTransformer,
+    #                        default_transformer_kwargs=rf_kwargs)
 
-    OF_kappa, OF_err = test(data_files[-1], reps, n_trees,
+    OF_kappa, OF_err = test(data_files[0], reps, n_trees,
                             default_transformer_class=ObliqueTreeClassificationTransformer,
                             default_transformer_kwargs=of_kwargs)
     
-    print("RF kappa, RF err:", RF_kappa, RF_err)
+    #print("RF kappa, RF err:", RF_kappa, RF_err)
     print("OF kappa, OF err:", OF_kappa, OF_err)
 
 if __name__ == "__main__":
