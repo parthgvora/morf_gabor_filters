1788efdc8f75a62bc70ab0c5cd36c685b9719299
parthgvora <parthgvora@gmail.com>
2020-12-07 18:47:02 -0500

    rerf sucks

--- test/comparison.py
+++ test/comparison.py
@@ -1,5 +1,6 @@
 import sys
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 
 from rerf.rerfClassifier import rerfClassifier
@@ -67,21 +68,31 @@ def visualize_data():
     plt.scatter(x[:, 0], x[:, 1], color=colourmap)
     plt.savefig("consistency")
 
+def get_data(n):
+    df_train = pd.read_csv("Sparse_parity_test.csv").to_numpy()
+    df_test = pd.read_csv("Sparse_parity_train.csv").to_numpy()
 
-def multitest(data, n, iters, clf, **clf_kwargs):
+    idx = np.random.choice(9999, n, replace=False)
+
+    X_train = df_train[idx, :-1]
+    y_train = df_train[idx, -1]
+    X_test = df_test[:, :-1]
+    y_test = df_test[:, -1]
+
+    return X_train, X_test, y_train, y_test
+
+
+def multitest(n, iters, clf, **clf_kwargs):
 
     acc = np.zeros(iters)
     for i in range(0, iters):
 
         c = clf(**clf_kwargs)
-        X, y = data(n)
+        X_train, X_test, y_train, y_test = get_data(n)
         
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, shuffle=True, stratify=y)
-        print(X_train.shape, X_test.shape)
-
         c.fit(X_train, y_train)
         y_hat = c.predict(X_test)
-        acc[i] = np.sum(y_hat == y_test) / n
+        acc[i] = 1 - np.sum(y_hat == y_test) / len(y_test)
     
     return np.mean(acc), np.std(acc)
 
@@ -92,15 +103,15 @@ def test_RF():
             "n_estimators" : 100
     }
 
+    acc, std = [], []
     print("Sparse Parity")
-    for n in [1000, 5000]: #, 10000]:
-        sparse_acc, sparse_std = multitest(sparse_parity, n, 3, RandomForestClassifier, **kwargs)
-        print(n, sparse_acc)
+    for n in [1000, 5000, 9999]:
+        sparse_acc, sparse_std = multitest(n, 3, RandomForestClassifier, **kwargs)
+        acc.append(sparse_acc)
+        std.append(sparse_std)
 
-    print("Orthant")
-    for n in [400, 2000, 4000]:
-        orth_acc, orth_std = multitest(orthant, n, 3, RandomForestClassifier, **kwargs)
-        print(n, orth_acc)
+    return acc, std
+
 
 def test_RerF():
 
@@ -109,49 +120,38 @@ def test_RerF():
             "projection_matrix": "RerF",
             "feature_combinations": 1.5,
     }
-    
+   
+    acc, std = [], []
     print("Sparse Parity")
-    for n in [1000, 5000]: #, 10000]:
-        sparse_acc, sparse_std = multitest(sparse_parity, n, 3, rerfClassifier, **kwargs)
-        print(n, sparse_acc)
+    for n in [1000, 5000, 9999]:
+        sparse_acc, sparse_std = multitest(n, 3, rerfClassifier, **kwargs)
+        acc.append(sparse_acc)
+        std.append(sparse_std)
 
-    print("Orthant")
-    for n in [400, 2000, 4000]:
-        orth_acc, orth_std = multitest(orthant, n, 3, rerfClassifier, **kwargs)
-        print(n, orth_acc)
-
-def test_OF():
-
-    kwargs = {
-            "n_estimators" : 100,
-            "feature_combinations": 1.5,
-            "density": 0.5
-    }
-    
-    print("Sparse Parity")
-    for n in [1000, 5000, 10000]:
-        sparse_acc, sparse_std = multitest(sparse_parity, n, 3, ObliqueForestClassifier, **kwargs)
-        print(n, sparse_acc)
-        break
-
-    print("Orthant")
-    for n in [400, 2000, 4000]:
-        orth_acc, orth_std = multitest(orthant, n, 3, ObliqueForestClassifier, **kwargs)
-        print(n, orth_acc)
-        break
-
-
+    return acc, std
 
 def main():
 
-    visualize_data()
+    #visualize_data()
     
-    #print("Random Forest")
-    #test_RF()
+    print("Random Forest")
+    RF_acc, RF_std = test_RF()
 
-    #print("Rerf")
-    #test_RerF()
+    print("Rerf")
+    Rerf_acc, Rerf_std = test_RerF()
 
+
+    plt.figure()
+    plt.plot([1, 3, 4], RF_acc, label="RF", color="Blue")
+    plt.errorbar([1, 3, 4], RF_acc, yerr=RF_std, ls="None", color="Blue")
+    plt.plot([1, 3, 4], Rerf_acc, label="RerF", color="Green")
+    plt.errorbar([1, 3, 4], Rerf_acc, yerr=Rerf_std, ls="None", color="Green")
+    plt.xticks([1, 3, 4], [1000, 5000, 10000])
+    plt.xlabel("Training samples")
+    plt.ylabel("Error")
+    plt.title("Sparse parity: RF vs RerF")
+    plt.legend()
+    plt.savefig("Sparse_Parity_RFvsRerF2")
     #print("Oblique Forest")
     #test_OF()
 
